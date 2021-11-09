39c45cabf6b286d0ca5af3ab7032317c22e45d77
oleg <simpltester@gmail.com>
2021-11-09 21:48:33 +0500

    провел работу над ошибками

--- src/square.py
+++
@@ -1,9 +0,0 @@
-from .figure import Figure
-
-class Square(Figure):
-    """class for square"""
-    def __init__(self, side):
-        super().__init__()
-        self.name = "Square"
-        self.area = side * side
-        self.perimeter = 4 * side
